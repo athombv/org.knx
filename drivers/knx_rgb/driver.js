e53c3f3618dbe0b24cdcce47b24b4bd081fba397
Kyle Taylor Parkins <kyle@athom.com>
2018-05-11 15:07:15 +0200

    Fixed connection at startup from drivers. Added additional dropdown boxes for groupaddresses. Added description from ETS export to groupaddress box.

--- drivers/knx_rgb/driver.js
+++ drivers/knx_rgb/driver.js
@@ -5,21 +5,6 @@ const KNXGenericDriver = require('./../../lib/generic_knx_driver.js');
 const uuidv4 = require('uuid/v4');
 
 class KNXRGB extends KNXGenericDriver {
-
-    onPairListDevices( data, callback ){
-
-        callback( null, [
-            {
-                name: 'KNX RGB device',
-                data: {
-                    id: uuidv4()
-                },
-                settings: {
-                    macAddress: "00246d0157"
-                }
-            }
-        ]);
-    }
 }
 
 module.exports = KNXRGB;
