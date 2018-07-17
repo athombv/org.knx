cba49bf31a56340f40fa9cf1c89acbdf9ad3bf45
Kyle Taylor Parkins <kyle@athom.com>
2018-07-17 15:52:12 +0200

    Fixed windowblinds stop function. Added windowblinds boolean status.

--- drivers/knx_windowcoverings/device.js
+++ drivers/knx_windowcoverings/device.js
@@ -12,14 +12,31 @@ class KNXWindowCovering extends KNXGeneric {
 
     onKNXEvent(groupaddress, data) {
         if (groupaddress === this.settings.ga_status) {
-            //this.setCapabilityValue('windowcoverings_state', DatapointTypeParser.upstopdown(data));
+            const state = DatapointTypeParser.onoff(data);
+            if (state) {
+                if (this.settings.invert_updown === true) {
+                    this.setCapabilityValue('windowcoverings_state', 'up');
+                } else {
+                    this.setCapabilityValue('windowcoverings_state', 'down');
+                }
+            } else {
+                if (this.settings.invert_updown === true) {
+                    this.setCapabilityValue('windowcoverings_state', 'down');
+                } else {
+                    this.setCapabilityValue('windowcoverings_state', 'up');
+                }
+            }
+            this.log(state);
         }
     }
 
     onCapabilityWindowCovering(value, opts) {
+        this.log(value);
         if (this.knxInterface) {
             switch(value) {
                 case 'up':
+                    if (this.settings.ga_stop) {this.knxInterface.writeKNXGroupAddress(this.settings.ga_stop, 0, 'DPT1')}
+
                     if (this.settings.invert_updown === true) {
                         return this.knxInterface.writeKNXGroupAddress(this.settings.ga_up_down, 1, 'DPT1')
                         .catch((knxerror) => {
@@ -31,6 +48,7 @@ class KNXWindowCovering extends KNXGeneric {
                         this.log(knxerror)
                     });
                 case 'down':
+                    if (this.settings.ga_stop) {this.knxInterface.writeKNXGroupAddress(this.settings.ga_stop, 0, 'DPT1')}
                     if (this.settings.invert_updown === true) {
                         return this.knxInterface.writeKNXGroupAddress(this.settings.ga_up_down, 0, 'DPT1')
                         .catch((knxerror) => {
@@ -41,6 +59,14 @@ class KNXWindowCovering extends KNXGeneric {
                     .catch((knxerror) => {
                         this.log(knxerror)
                     });
+                case 'idle':
+                    this.log('stop selected, stop address', this.settings.ga_stop);
+                    if(this.settings.ga_stop) {
+                        return this.knxInterface.writeKNXGroupAddress(this.settings.ga_stop, 1, 'DPT1')
+                        .catch((knxerror) => {
+                            this.log(knxerror);
+                        })
+                    }
             }
         }
     }
