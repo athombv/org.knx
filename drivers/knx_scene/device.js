2fd9762e8c6c8aab59a0c247655a940ce1893e32
Kyle Taylor Parkins <kyle@athom.com>
2018-12-14 13:19:48 +0100

    Changed UDP methods in interfacemanager. Added more error handling for the conncetion. Added logging for every send KNX telegram. Added additional flows to scenes.

--- drivers/knx_scene/device.js
+++ drivers/knx_scene/device.js
@@ -9,28 +9,37 @@ class KNXScene extends KNXGeneric {
         super.onInit();
         this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this));
 
-        let triggerSceneAction = new Homey.FlowCardAction('trigger_scene');
+        let triggerSceneAction = new Homey.FlowCardAction('trigger_to_scene');
         triggerSceneAction
             .register()
             .registerRunListener((args, state) => {
-                return this.triggerScene();
+                return this.triggerToScene();
             });
+
+        let triggerFlowFromScene = new Homey.FlowCardTrigger('trigger_from_scene');
+        triggerFlowFromScene.register();
     }
 
     onKNXEvent(groupaddress, data) {
         super.onKNXEvent(groupaddress, data);
+        
         if (groupaddress === this.settings.ga_scene) {
             this.setCapabilityValue('onoff', DatapointTypeParser.onoff(data));
+            // Trigger any flow that is bound on this device.
+            triggerFlowFromScene.trigger()
+                .catch(error)
         }
     }
 
     onCapabilityOnoff(value, opts) {
         if (this.knxInterface && this.settings.ga_scene && value === true) {
-            return this.triggerScene();
+            return this.triggerToScene();
         }
     }
 
-    triggerScene() {
+    // This function triggers a scene from Homey.
+    // Either from the button in de device overview or from a flow card.
+    triggerToScene() {
         return this.knxInterface.writeKNXGroupAddress(this.settings.ga_scene, (this.settings.scene_number -1), 'DPT17') // The -1 is temporary until the knx.js lib fully supports scenes
         .catch((knxerror) => {
             this.log(knxerror);
