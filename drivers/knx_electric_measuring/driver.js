cfd0f6f3a1af7671d97bea9598af2d48649bd803
Kyle Athom <kyle@athom.nl>
2019-12-19 14:45:54 +0100

    Version 1.1.0
    KNX library updated, lots of connection and small protocol fixes.
    Temporary removed kwh measurement device for this release.

--- drivers/knx_electric_measuring/driver.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const KNXGenericDriver = require('./../../lib/GenericKNXDriver');
-
-class KNXElectricMeasuring extends KNXGenericDriver {
-}
-
-module.exports = KNXElectricMeasuring;
