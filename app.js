e7a4d96ee7ec0b2f7439a6627c6202622969497a
petero-dk <2478689+petero-dk@users.noreply.github.com>
2024-06-03 11:17:20 +0200

    flow trigger for telegram listen

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const KNXInterfaceManager = require('./lib/KNXInterfaceManager');
 class KNXApp extends Homey.App {
 
   async onInit() {
+
     this.log('KNXApp is running...');
     const address = await this.homey.cloud.getLocalAddress();
     const homeyIP = address.split(':', 1).toString();
@@ -13,50 +14,97 @@ class KNXApp extends Homey.App {
     this.log('Homey IP + Parsed IP', address, homeyIP);
     this.knxInterfaceManager = new KNXInterfaceManager(homeyIP);
 
-    
 
     const sendTelegramAction = this.homey.flow.getActionCard('knx_send');
-    sendTelegramAction.registerRunListener(async (args, state) => {
+    sendTelegramAction.registerRunListener(sendTelegramAction.bind(this));
 
-      console.log(args);
-      console.log(state);
 
-      let knxInterfaceToUse = this.knxInterfaceManager.getKNXInterface(args.interface.mac);
+    this.recieveTelegramTrigger = this.homey.flow.getTriggerCard('knx_receive');
+    this.eventListernerGroupAddresses = [];
 
-      if (args.interface.mac === 'any') {
-        const availableInterfaces = this.knxInterfaceManager.getKNXInterfaceList();
-        if (availableInterfaces.length > 0) {
-          knxInterfaceToUse = availableInterfaces[0];
-        }
-      }
+    recieveTelegramTrigger.registerRunListener(async (args, state) => {
+      return args.group_address === state.group_address && (args.interface === "[any]" || args.interface === state.interface);
+    });
 
-      if (!knxInterfaceToUse) 
-        return Promise.reject('Interface not found');
+    recieveTelegramTrigger.getArgumentValues().then(this.registerKNXEventHandlers.bind(this));
 
-      
-      if (!args.group_address) 
-        return Promise.reject('No group address selected');
-      
-      if (args.data_type == "none") {
-        knxInterfaceToUse.writeKNXGroupAddress(args.group_address, args.value);
-      } else {
-        knxInterfaceToUse.writeKNXGroupAddress(args.group_address, args.value, args.data_type);
+    recieveTelegramTrigger.on("update", () => {
+      this.log("update knx flow arguments");
+      recieveTelegramTrigger.getArgumentValues().then(this.registerKNXEventHandlers.bind(this));
+    });
+
+    sendTelegramAction.registerArgumentAutocompleteListener("interface", this.interfaceAutocomplete.bind(this));
+    recieveTelegramTrigger.registerArgumentAutocompleteListener("interface", this.interfaceAutocomplete.bind(this));
+
+  }
+
+  // referenced via this.KNXEventHandler
+  onKNXEvent(interfaceMac, groupaddress, data) {
+    const tokens = { value_number: data, value_bool: data > 0 };
+    const state = { group_address: groupaddress, interface: interfaceMac };
+
+    // trigger the card
+    recieveTelegramTrigger.trigger(tokens, state)
+      .then(this.log)
+      .catch(this.error);
+  }
+
+  registerKNXEventHandlers(args) {
+    this.log("args", args);
+
+    args.forEach(event => {
+      if (!this.eventListernerGroupAddresses.includes(event.interface + "-" +event.group_address)) {
+        this.eventListernerGroupAddresses.push(event.interface + "-" + event.group_address);
+        // Store the event listener so we can remove it later
+        this.knxInterface.addKNXEventListener(settings.ga_status, this.onKNXEvent.bind( this, event.interface));
       }
 
     });
-    
-    sendTelegramAction.registerArgumentAutocompleteListener(
-      "interface",
-      async (query, args) => {
-        const results = this.knxInterfaceManager.getSimpleInterfaceList();
-        results.push({name: '[any]', mac: 'any', ip: 'any'});
 
-        // filter based on the query
-        return results.filter((result) => {
-           return result.name.toLowerCase().includes(query.toLowerCase()) || result.mac.toLowerCase().includes(query.toLowerCase()) || result.ip.toLowerCase().includes(query.toLowerCase()); 
-        });
+    this.eventListernerGroupAddresses.forEach(eventListernerId => {
+      if (args.filter(event => event.interface + "-" + event.group_address === eventListernerId).length === 0) {
+        //TODO: figuure out how to remove the event listener, because we need the correct event listener
+        //this.knxInterface.removeKNXEventListener(groupAddress, this.onKNXEvent.bind( this, event.interface));
       }
-    );
+    });
+  }
+
+  async interfaceAutocomplete(query, args) {
+    const results = this.knxInterfaceManager.getSimpleInterfaceList();
+    results.push({ name: '[any]', mac: 'any', ip: 'any' });
+
+    // filter based on the query
+    return results.filter((result) => {
+      return result.name.toLowerCase().includes(query.toLowerCase()) || result.mac.toLowerCase().includes(query.toLowerCase()) || result.ip.toLowerCase().includes(query.toLowerCase());
+    });
+  }
+
+  async sendTelegramAction(args, state) {
+
+    console.log(args);
+    console.log(state);
+
+    let knxInterfaceToUse = this.knxInterfaceManager.getKNXInterface(args.interface.mac);
+
+    if (args.interface.mac === 'any') {
+      const availableInterfaces = this.knxInterfaceManager.getKNXInterfaceList();
+      if (availableInterfaces.length > 0) {
+        knxInterfaceToUse = availableInterfaces[0];
+      }
+    }
+
+    if (!knxInterfaceToUse)
+      return Promise.reject('Interface not found');
+
+
+    if (!args.group_address)
+      return Promise.reject('No group address selected');
+
+    if (args.data_type == "none") {
+      knxInterfaceToUse.writeKNXGroupAddress(args.group_address, args.value);
+    } else {
+      knxInterfaceToUse.writeKNXGroupAddress(args.group_address, args.value, args.data_type);
+    }
 
   }
 
