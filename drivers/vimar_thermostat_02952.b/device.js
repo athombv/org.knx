f48552ad2f429c8962a98f6e4d8cf6efade93cbe
petero-dk <2478689+petero-dk@users.noreply.github.com>
2024-06-12 22:23:04 +0200

    promise to async, adjust capabilities based on settings

--- drivers/vimar_thermostat_02952.b/device.js
+++ drivers/vimar_thermostat_02952.b/device.js
@@ -21,60 +21,67 @@ class VimarThermostat02952BDevice extends KNXGenericDevice {
     this.registerCapabilityListener('target_temperature', this.onCapabilityTargetTemperature.bind(this));
     this.registerCapabilityListener('vimar_thermostat_mode', this.onCapabilityMode.bind(this));
 
+    // Maybe this can be placed better during pairing?
+    if (!this.settings.ga_summerwinter_state && this.hasCapability('summer_winter')) {
+      this.removeCapability('summer_winter');
+      this.knxInterface.removeKNXConnectionListener(this.settings.ga_summerwinter_state, this.KNXEventHandler);
+    }
+    if (!this.settings.ga_valve_heating_state && this.hasCapability('valve_heating')) {
+      this.removeCapability('valve_heating');
+      this.knxInterface.removeKNXConnectionListener(this.settings.ga_valve_heating_state, this.KNXEventHandler);
+    }
+    if (!this.settings.ga_valve_cooling_state && this.hasCapability('valve_cooling')) {
+      this.removeCapability('valve_cooling');
+      this.knxInterface.removeKNXConnectionListener(this.settings.ga_valve_cooling_state, this.KNXEventHandler);
+    }
   }
 
-  onKNXEvent(groupaddress, data) {
+  async onKNXEvent(groupaddress, data) {
     super.onKNXEvent(groupaddress, data);
 
-    if (groupaddress === this.settings.ga_temperature_target) {
-      this.currentSetpointShift = DatapointTypeParser.dpt9(data);
-      setTimeout(() => {
-        this.currentSetpointShift = undefined;
-      }, 500);
-    }
-    if (groupaddress === this.settings.ga_temperature_target_actual) {
-      this.currentSetpoint = DatapointTypeParser.dpt9(data);
-      this.setCapabilityValue('target_temperature', this.currentSetpoint)
-        .catch((knxerror) => {
-          this.log('Set target_temperature error', knxerror);
-        });
-      setTimeout(() => {
-        this.currentSetpoint = undefined;
-      }, 500);
-    }
-
-    if (this.currentSetpoint !== undefined && this.currentSetpointShift !== undefined) {
-      this.currentSetpointBase = this.currentSetpoint - this.currentSetpointShift;
-    }
-
-    if (groupaddress === this.settings.ga_temperature_measure) {
-      this.setCapabilityValue('measure_temperature', DatapointTypeParser.dpt9(data))
-        .catch((knxerror) => this.log('Set measure_temperature error', knxerror));
-    }
-    if (groupaddress === this.settings.ga_valve_heating_state) {
-      this.setCapabilityValue('valve_heating', DatapointTypeParser.onoff(data))
-        .catch((knxerror) => this.log('Set valve_heating error', knxerror));
-    }
-    if (groupaddress === this.settings.ga_valve_cooling_state) {
-      this.setCapabilityValue('valve_cooling', DatapointTypeParser.onoff(data))
-        .catch((knxerror) => this.log('Set valve_cooling error', knxerror));
-    }
-    if (groupaddress === this.settings.ga_summerwinter_state) {
-      this.setCapabilityValue('summer_winter', DatapointTypeParser.onoff(data) ? 'Winter' : 'Summer')
-        .catch((knxerror) => this.log('Set summer_winter error', knxerror));
-    }
-    if (groupaddress === this.settings.ga_thermostat_mode_state) {
-      if (this.settings.ga_temperature_measure) {
-        this.knxInterface.readKNXGroupAddress(this.settings.ga_temperature_measure)
-          .catch((knxerror) => this.log(knxerror));
+    try {
+      if (groupaddress === this.settings.ga_temperature_target) {
+        this.currentSetpointShift = DatapointTypeParser.dpt9(data);
+        setTimeout(() => {
+          this.currentSetpointShift = undefined;
+        }, 500);
       }
-      if (this.settings.ga_temperature_target_actual) {
-        this.knxInterface.readKNXGroupAddress(this.settings.ga_temperature_target_actual)
-          .catch((knxerror) => this.log(knxerror));
+      if (groupaddress === this.settings.ga_temperature_target_actual) {
+        this.currentSetpoint = DatapointTypeParser.dpt9(data);
+        await this.setCapabilityValue('target_temperature', this.currentSetpoint);
+        setTimeout(() => {
+          this.currentSetpoint = undefined;
+        }, 500);
       }
 
-      this.setCapabilityValue('vimar_thermostat_mode', `${DatapointTypeParser.dpt17(data)}`)
-        .catch((knxerror) => this.log('Set vimar_thermostat_mode error', knxerror));
+      if (this.currentSetpoint !== undefined && this.currentSetpointShift !== undefined) {
+        this.currentSetpointBase = this.currentSetpoint - this.currentSetpointShift;
+      }
+
+      if (groupaddress === this.settings.ga_temperature_measure) {
+        await this.setCapabilityValue('measure_temperature', DatapointTypeParser.dpt9(data));
+      }
+      if (groupaddress === this.settings.ga_valve_heating_state) {
+        await this.setCapabilityValue('valve_heating', DatapointTypeParser.onoff(data));
+      }
+      if (groupaddress === this.settings.ga_valve_cooling_state) {
+        await this.setCapabilityValue('valve_cooling', DatapointTypeParser.onoff(data));
+      }
+      if (groupaddress === this.settings.ga_summerwinter_state) {
+        await this.setCapabilityValue('summer_winter', DatapointTypeParser.onoff(data) ? 'Winter' : 'Summer');
+      }
+      if (groupaddress === this.settings.ga_thermostat_mode_state) {
+        if (this.settings.ga_temperature_measure) {
+          await this.knxInterface.readKNXGroupAddress(this.settings.ga_temperature_measure);
+        }
+        if (this.settings.ga_temperature_target_actual) {
+          await this.knxInterface.readKNXGroupAddress(this.settings.ga_temperature_target_actual);
+        }
+
+        await this.setCapabilityValue('vimar_thermostat_mode', `${DatapointTypeParser.dpt17(data)}`);
+      }
+    } catch (error) {
+      this.log('Error in onKNXEvent', error);
     }
   }
 
@@ -89,70 +96,72 @@ class VimarThermostat02952BDevice extends KNXGenericDevice {
     this.knxInterface.addKNXEventListener(settings.ga_valve_cooling_state, this.KNXEventHandler);
   }
 
-  onKNXConnection(connectionStatus) {
+  async onKNXConnection(connectionStatus) {
     super.onKNXConnection(connectionStatus);
 
-    if (connectionStatus === 'connected') {
-      // Reading the groupaddress will trigger a event on the bus.
-      // This will be catched by onKNXEvent, hence the return value is not used.
-      if (this.settings.ga_temperature_target) {
-        this.knxInterface.readKNXGroupAddress(this.settings.ga_temperature_target)
-          .catch((knxerror) => this.log(knxerror));
+    try {
+      if (connectionStatus === 'connected') {
+        // Reading the groupaddress will trigger a event on the bus.
+        // This will be catched by onKNXEvent, hence the return value is not used.
+        if (this.settings.ga_temperature_target) {
+          await this.knxInterface.readKNXGroupAddress(this.settings.ga_temperature_target);
+        }
+        if (this.settings.ga_temperature_measure) {
+          await this.knxInterface.readKNXGroupAddress(this.settings.ga_temperature_measure);
+        }
+        if (this.settings.ga_temperature_target_actual) {
+          await this.knxInterface.readKNXGroupAddress(this.settings.ga_temperature_target_actual);
+        }
+        if (this.settings.ga_thermostat_mode_state) {
+          await this.knxInterface.readKNXGroupAddress(this.settings.ga_thermostat_mode_state);
+        }
+        if (this.settings.ga_summerwinter_state) {
+          await this.knxInterface.readKNXGroupAddress(this.settings.ga_summerwinter_state);
+        }
+        if (this.settings.ga_valve_heating_state) {
+          await this.knxInterface.readKNXGroupAddress(this.settings.ga_valve_heating_state);
+        }
+        if (this.settings.ga_valve_cooling_state) {
+          await this.knxInterface.readKNXGroupAddress(this.settings.ga_valve_cooling_state);
+        }
       }
-      if (this.settings.ga_temperature_measure) {
-        this.knxInterface.readKNXGroupAddress(this.settings.ga_temperature_measure)
-          .catch((knxerror) => this.log(knxerror));
-      }
-      if (this.settings.ga_temperature_target_actual) {
-        this.knxInterface.readKNXGroupAddress(this.settings.ga_temperature_target_actual)
-          .catch((knxerror) => this.log(knxerror));
-      }
-      if (this.settings.ga_thermostat_mode_state) {
-        this.knxInterface.readKNXGroupAddress(this.settings.ga_thermostat_mode_state)
-          .catch((knxerror) => this.log(knxerror));
-      }
-      if (this.settings.ga_summerwinter_state) {
-        this.knxInterface.readKNXGroupAddress(this.settings.ga_summerwinter_state)
-          .catch((knxerror) => this.log(knxerror));
-      }
-      if (this.settings.ga_valve_heating_state) {
-        this.knxInterface.readKNXGroupAddress(this.settings.ga_valve_heating_state)
-          .catch((knxerror) => this.log(knxerror));
-      }
-      if (this.settings.ga_valve_cooling_state) {
-        this.knxInterface.readKNXGroupAddress(this.settings.ga_valve_cooling_state)
-          .catch((knxerror) => this.log(knxerror));
-      }
+    } catch (knxerror) {
+      this.log('Failed to read', knxerror);
     }
   }
 
-  onCapabilityMode(value, opts) {
+  async onCapabilityMode(value, opts) {
     if (this.knxInterface && this.settings.ga_thermostat_mode) {
-      return this.knxInterface.writeKNXGroupAddress(this.settings.ga_thermostat_mode, value, 'DPT17')
-        .catch((knxerror) => {
-          this.log(knxerror);
-          throw new Error(this.homey.__('errors.mode_set_failed'));
-        }).then(() => new Promise((resolve) => setTimeout(resolve, 400))).then(() => {
-          if (this.settings.ga_temperature_measure) {
-            this.knxInterface.readKNXGroupAddress(this.settings.ga_temperature_measure)
-              .catch((knxerror) => this.log(knxerror));
-          }
-          if (this.settings.ga_temperature_target_actual) {
-            this.knxInterface.readKNXGroupAddress(this.settings.ga_temperature_target_actual)
-              .catch((knxerror) => this.log(knxerror));
-          }
-        });
+      try {
+        await this.knxInterface.writeKNXGroupAddress(this.settings.ga_thermostat_mode, value, 'DPT17');
+      } catch (knxerror) {
+        this.log(knxerror);
+        throw new Error(this.homey.__('errors.mode_set_failed'));
+      }
+
+      // Wait a bit before reading the temperature,
+      // as the mode change will trigger a new setpoint
+      // the knx lib has an issue when sending read requests too fast after a write,
+      // it will actually send them before
+      await (new Promise((resolve) => setTimeout(resolve, 400)));
+
+      if (this.settings.ga_temperature_measure) {
+        await this.knxInterface.readKNXGroupAddress(this.settings.ga_temperature_measure);
+      }
+      if (this.settings.ga_temperature_target_actual) {
+        await this.knxInterface.readKNXGroupAddress(this.settings.ga_temperature_target_actual);
+      }
     }
-    return null;
   }
 
   onCapabilityTargetTemperature(value, opts) {
     this.getMeasuredTemperature();
     if (this.knxInterface && this.settings.ga_temperature_target) {
+      if (this.currentSetpointBase === undefined) {
+        throw new Error('Actual setpoint and shift are unknown');
+      }
 
-      // TODO: Some error handling here if currentSetpointBase is undefined
       const newShift = value - this.currentSetpointBase;
-      console.log('New shift', value, newShift, this.currentSetpoint, this.currentSetpointShift);
       return this.knxInterface.writeKNXGroupAddress(this.settings.ga_temperature_target, newShift, 'DPT9.1')
         .catch((knxerror) => {
           this.log(knxerror);
@@ -169,6 +178,46 @@ class VimarThermostat02952BDevice extends KNXGenericDevice {
           this.log(knxerror);
           throw new Error(this.homey.__('errors.temperature_get_failed'));
         });
+    }
+  }
+
+  /**
+   * onSettings is called when the user updates the device's settings.
+   * @param {object} event the onSettings event data
+   * @param {object} event.oldSettings The old settings object
+   * @param {object} event.newSettings The new settings object
+   * @param {string[]} event.changedKeys An array of keys changed since the previous version
+   * @returns {Promise<string|void>} return a custom message that will be displayed
+   */
+  async onSettings({ oldSettings, newSettings, changedKeys }) {
+    await super.onSettings({ oldSettings, newSettings, changedKeys });
+
+    if (!newSettings.ga_summerwinter_state && this.hasCapability('summer_winter')) {
+      this.removeCapability('summer_winter');
+      if (oldSettings.ga_summerwinter_state) {
+        this.knxInterface.removeKNXConnectionListener(oldSettings.ga_summerwinter_state, this.KNXEventHandler);
+      }
+    } else if (newSettings.ga_summerwinter_state && !this.hasCapability('summer_winter')) {
+      this.addCapability('summer_winter');
+      this.knxInterface.addKNXEventListener(newSettings.ga_summerwinter_state, this.KNXEventHandler);
+    }
+    if (!newSettings.ga_valve_heating_state && this.hasCapability('valve_heating')) {
+      this.removeCapability('valve_heating');
+      if (oldSettings.ga_valve_heating_state) {
+        this.knxInterface.removeKNXConnectionListener(oldSettings.ga_valve_heating_state, this.KNXEventHandler);
+      }
+    } else if (newSettings.ga_valve_heating_state && !this.hasCapability('valve_heating')) {
+      this.addCapability('valve_heating');
+      this.knxInterface.addKNXEventListener(newSettings.ga_valve_heating_state, this.KNXEventHandler);
+    }
+    if (!newSettings.ga_valve_cooling_state && this.hasCapability('valve_cooling')) {
+      this.removeCapability('valve_cooling');
+      if (oldSettings.ga_valve_cooling_state) {
+        this.knxInterface.removeKNXConnectionListener(oldSettings.ga_valve_cooling_state, this.KNXEventHandler);
+      }
+    } else if (newSettings.ga_valve_cooling_state && !this.hasCapability('valve_cooling')) {
+      this.addCapability('valve_cooling');
+      this.knxInterface.addKNXEventListener(newSettings.ga_valve_cooling_state, this.KNXEventHandler);
     }
   }
 
--- drivers/vimar_thermostat_02952.b/driver.js
+++ drivers/vimar_thermostat_02952.b/driver.js
@@ -4,17 +4,18 @@ const KNXGenericDriver = require('../../lib/GenericKNXDriver');
 
 class VimarThermostat02952BDriver extends KNXGenericDriver {
 
-    onInit() {
-        super.onInit();
+  onInit() {
+    super.onInit();
 
-        this.homey.flow.getActionCard('set-window-switch').registerRunListener(async (args, state) => {
-            return args.device.knxInterface.writeKNXGroupAddress(args.device.settings.ga_window_switch, args.open, 'DPT1')
-                .catch((knxerror) => {
-                    this.log(knxerror);
-                    throw new Error(this.homey.__('errors.window_switch_failed'));
-                });
+    this.homey.flow.getActionCard('set-window-switch').registerRunListener(async (args, state) => {
+      return args.device.knxInterface.writeKNXGroupAddress(args.device.settings.ga_window_switch, args.open, 'DPT1')
+        .catch((knxerror) => {
+          this.log(knxerror);
+          throw new Error(this.homey.__('errors.window_switch_failed'));
         });
-    }
+    });
+  }
+
 }
 
 module.exports = VimarThermostat02952BDriver;
