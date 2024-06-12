210cab6929e10a50d73c53818aeb34bc5f6c58bd
petero-dk <2478689+petero-dk@users.noreply.github.com>
2024-06-13 00:24:24 +0200

    Add new flow action
    That can set the shift to 0 and thus resetting the setpoint to the base of current mode

--- drivers/vimar_thermostat_02952.b/driver.js
+++ drivers/vimar_thermostat_02952.b/driver.js
@@ -14,6 +14,14 @@ class VimarThermostat02952BDriver extends KNXGenericDriver {
           throw new Error(this.homey.__('errors.window_switch_failed'));
         });
     });
+
+    this.homey.flow.getActionCard('reset_to_basesetpoint').registerRunListener(async (args, state) => {
+      return args.device.knxInterface.writeKNXGroupAddress(args.device.settings.ga_temperature_target, 0, 'DPT9.1')
+        .catch((knxerror) => {
+          this.log(knxerror);
+          throw new Error(this.homey.__('errors.reset_to_basesetpoint_failed'));
+        });
+    });
   }
 
 }
