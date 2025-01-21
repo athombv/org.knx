cf9bdc5e009ba45c476a53c29763e2713b3d55c9
Sébastien GLON <sebglon@gmail.com>
2025-01-21 21:38:19 +0100

    remove home_alarm_state

--- drivers/knx_home_alarm_state/device.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const KNXGenericSensor = require('../../lib/GenericKNXSensor');
-
-module.exports = class KNXAlarmHomeState extends KNXGenericSensor {
-
-};
--- drivers/knx_home_alarm_state/driver.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const KNXGenericDriver = require('../../lib/GenericKNXDriver');
-
-module.exports = class KNXHomeAlarmState extends KNXGenericDriver {
-};
