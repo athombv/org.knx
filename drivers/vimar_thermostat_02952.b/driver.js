061a6a4529ad7517ef530f6b941a611c56a50c89
petero-dk <2478689+petero-dk@users.noreply.github.com>
2025-08-29 10:54:28 -0700

    Check for window switch GA, display optional for optional GAs

--- drivers/vimar_thermostat_02952.b/driver.js
+++ drivers/vimar_thermostat_02952.b/driver.js
@@ -8,11 +8,12 @@ class VimarThermostat02952BDriver extends KNXGenericDriver {
     super.onInit();
 
     this.homey.flow.getActionCard('set-window-switch').registerRunListener(async (args, state) => {
-      return args.device.knxInterface.writeKNXGroupAddress(args.device.settings.ga_window_switch, args.open, 'DPT1')
-        .catch((knxerror) => {
-          this.log(knxerror);
-          throw new Error(this.homey.__('errors.window_switch_failed'));
-        });
+      if (args.device.settings.ga_window_switch)
+        return args.device.knxInterface.writeKNXGroupAddress(args.device.settings.ga_window_switch, args.open, 'DPT1')
+          .catch((knxerror) => {
+            this.log(knxerror);
+            throw new Error(this.homey.__('errors.window_switch_failed'));
+          });
     });
 
     this.homey.flow.getActionCard('reset_to_basesetpoint').registerRunListener(async (args, state) => {
